408fc32ad34a819c3c6057b18ee60cbcd921a5bd
Bo Trethewey <botrethewey@gmail.com>
2017-02-19 00:17:42 -0800

    Added comments.

--- 01-leap/leap.rb
+++ 01-leap/leap.rb
@@ -1,5 +1,6 @@
 def leap_year? year
-  #must be divisible by 4
+  #handle false cases: when the passed year is not divisible by 4
+  #OR the year is greater than equal to 400 AND divisible by 4 and 100 but not divisible by 400.
   if ( year % 4 != 0 ) || ( year >= 400 && ( year % 100 == 0 && year % 400 != 0 ) )
     return false
   else
