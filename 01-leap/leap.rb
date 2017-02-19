ef4f3b1a6be8d865ae1b044279b902ffa499f550
Bo Trethewey <botrethewey@gmail.com>
2017-02-19 00:08:19 -0800

    Final ver: passed the test

--- 01-leap/leap.rb
+++ 01-leap/leap.rb
@@ -1,8 +1,8 @@
 def leap_year? year
   #must be divisible by 4
-  if ( year % 4 == 0 ) && ( year < 400 || ( year % 100 == 0 && year % 400 == 0 ) )
-    return true
-  else
+  if ( year % 4 != 0 ) || ( year >= 400 && ( year % 100 == 0 && year % 400 != 0 ) )
     return false
+  else
+    return true
   end
 end
