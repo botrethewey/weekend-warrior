8f80686db7453cbbef33a536547ab985432aedaf
Bo Trethewey <botrethewey@gmail.com>
2017-02-19 00:24:15 -0800

    Removed redundant boolean expression for the conditional.

--- 01-leap/leap.rb
+++ 01-leap/leap.rb
@@ -1,7 +1,7 @@
 def leap_year? year
   #handle false cases: when the passed year is not divisible by 4
-  #OR the year is greater than equal to 400 AND divisible by 4 and 100 but not divisible by 400.
-  if ( year % 4 != 0 ) || ( year >= 400 && ( year % 100 == 0 && year % 400 != 0 ) )
+  #OR the year is divisible by 4 and 100 but not divisible by 400.
+  if ( year % 4 != 0 ) || ( year % 100 == 0 && year % 400 != 0 )
     return false
   else
     return true
