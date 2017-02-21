eb3a97c7130017dc3433757e2540f2aafa339af0
Bo Trethewey <botrethewey@gmail.com>
2017-02-21 10:30:24 -0800

    Used more efficient logic

--- 01-leap/leap.rb
+++ 01-leap/leap.rb
@@ -1,9 +1,15 @@
 def leap_year? year
   #handle false cases: when the passed year is not divisible by 4
   #OR the year is divisible by 4 and 100 but not divisible by 400.
-  if ( year % 4 != 0 ) || ( year % 100 == 0 && year % 400 != 0 )
-    return false
-  else
-    return true
-  end
+  # if ( year % 4 != 0 ) || ( year % 100 == 0 && year % 400 != 0 )
+  #   return false
+  # else
+  #   return true
+  # end
+
+  #One line logical operation
+  #return year % 4 == 0 && ( year % 100 != 0 || year % 400 == 0 )
+
+  #no need for return
+  year % 4 == 0 && ( year % 100 != 0 || year % 400 == 0 )
 end
